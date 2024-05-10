5f2216a14b088c9b7fd6d8a3ac1cc0adbe83db00
Grant Cummings <grantcummings4429@gmail.com>
2024-05-10 08:59:31 -0400

    finished lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,27 @@
-// Your tests here
+import { isPalindrome } from '../utils'
+
+describe("isPalindrome", () => {
+    it("returns true if the word that is tested is spelled exacly the same if the characters were reversed", () => {
+        const word = "racecar"
+
+        const palindrome = isPalindrome(word)
+
+        expect(palindrome).toBe(true)
+    })
+
+    it("returns true if the word that is tested is spelled exacly the same if the characters were reversed", () => {
+        const word = "test"
+
+        const palindrome = isPalindrome(word)
+
+        expect(palindrome).toBe(false)
+    })
+
+    it("returns true if the word that is tested is spelled exacly the same if the characters were reversed", () => {
+        const word = "hAnnaH"
+
+        const palindrome = isPalindrome(word)
+
+        expect(palindrome).toBe(true)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,11 @@
-// Your code here
+
+export function isPalindrome(word) {
+    let updatedWord = word.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    let reversedWord = updatedWord.split('').reverse().join('');
+    
+    if (reversedWord === word.toLowerCase()) {
+        return true
+    } else {
+        return false
+    }
+}
